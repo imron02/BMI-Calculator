76c636744f7c2d7b72a8f791f2916a2f67df2e45
Imron02 <imron02@gmail.com>
2021-04-27 11:45:50 +0700

    Refactor(header_component): update color from util

--- src/components/header_component.js
+++ src/components/header_component.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   title: {
-    color: '#fff',
+    color: Colors.white,
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 16,
